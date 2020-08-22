50bb1692d448fcd7380820909e61f4117c311322
Nikita <nikita.rabchuk@gmail.com>
2020-08-22 19:01:31 +0300

    fix git

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -16,9 +16,14 @@ function toColumn(col) {
 }
 
 function createRow(index, content) {
+    const resize = index ? '<div class="row-resize" data-resize="row"></div>'
+        : '';
     return `
-        <div class="row">
-            <div class="row-info">${index ? index : ''}</div>
+    <div class="row" data-type="resize">
+        <div class="row-info">
+            ${index ? index : ''}
+            ${resize}
+        </div>
             <div class="row-data">${content}</div>
         </div>
     `;
